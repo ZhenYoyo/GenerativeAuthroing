ad4b17a1f018c60ecf626bfcba7adb8498f8fdc1
ZhenYoyo <zwuch@connect.ust.hk>
2024-03-12 23:18:47 +0800

    Create langchain_readwebsiteinfo.py

---
+++ langchain_readwebsiteinfo.py
@@ -0,0 +1,67 @@
+
+import os
+from langchain_openai import OpenAI
+
+API_SECRET_KEY = "sk-f29A4tQuVfHMbdcgqHrNIF3QRVJmbpnrqucE7V7062r4fz8L"
+BASE_URL = "https://api.f2gpt.com/v1" 
+
+os.environ["OPENAI_API_KEY"] = API_SECRET_KEY
+os.environ["OPENAI_API_BASE"] = BASE_URL
+
+llm = OpenAI(temperature=0.9)
+from langchain_core.prompts import ChatPromptTemplate
+
+from langchain_community.document_loaders import WebBaseLoader
+loader = WebBaseLoader("https://paperarchipelago.wordpress.com/technical-log/")
+
+docs = loader.load()
+
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings()
+
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}""")
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+
+from langchain.chains import create_retrieval_chain
+
+retriever = vector.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+response = retrieval_chain.invoke({"input": "what is this web about?"})
+print(response["answer"])
+
+
+
+# LangSmith offers several features that can help with testing:...
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "You are world class technical documentation writer."),
+#     ("user", "{input}")
+# ])
+
+# from langchain_core.output_parsers import StrOutputParser
+
+# output_parser = StrOutputParser()
+
+
+# chain = prompt | llm | output_parser
+# result=chain.invoke({"input": "say hello to me"})
+# print(result)
